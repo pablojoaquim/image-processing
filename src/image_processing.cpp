64d65fc40a892419706af11dcb2a6536e1b21cec
pablo.joaquim <pablo.joaquim@gmail.com>
2026-01-09 13:07:19 -0300

    Binary conversion of the grayscale image

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -79,6 +79,7 @@ int opencv_test()
         return -1;
     }
 
+    std::cout << "Image info: " << std::endl;
     std::cout << "Size: " << img.cols << " x " << img.rows << std::endl;
     std::cout << "Channels: " << img.channels() << std::endl;
     std::cout << "Type: " << img.type() << std::endl;
@@ -97,7 +98,33 @@ int opencv_test()
     // Grayscale conversion (this is very useful for objects and borders detection)
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY); // cvtColor: convert between color spaces
+    std::cout << "Gray Image info: " << std::endl;
+    std::cout << "Size: " << gray.cols << " x " << gray.rows << std::endl;
+    std::cout << "Channels: " << gray.channels() << std::endl;
+    std::cout << "Type: " << gray.type() << std::endl;
     cv::imshow("Gray", gray);
+    cv::waitKey(0);
+
+    // Binary conversion
+    cv::Mat binary;
+    // For every pixel, the same threshold value is applied. If the pixel value is 
+    // smaller than or equal to the threshold, it is set to 0, otherwise it is set to a maximum value
+    cv::threshold(gray, binary, 128, 255, cv::THRESH_BINARY);
+    std::cout << "Binary Image info: " << std::endl;
+    std::cout << "Size: " << binary.cols << " x " << binary.rows << std::endl;
+    std::cout << "Channels: " << binary.channels() << std::endl;
+    std::cout << "Type: " << binary.type() << std::endl;
+    cv::imshow("Binary", binary);
+    cv::waitKey(0);
+
+    // Binary conversion with the best threshold accordint to the histogram of the image
+    cv::Mat binaryOtsu;
+    cv::threshold(gray, binaryOtsu, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);
+    std::cout << "Binary Otsu Image info: " << std::endl;
+    std::cout << "Size: " << binaryOtsu.cols << " x " << binaryOtsu.rows << std::endl;
+    std::cout << "Channels: " << binaryOtsu.channels() << std::endl;
+    std::cout << "Type: " << binaryOtsu.type() << std::endl;
+    cv::imshow("Binary Otsu", binaryOtsu);
     cv::waitKey(0);
 
     cv::destroyAllWindows();
